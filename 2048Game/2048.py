a571e2c7aafb5532387b237f7f53be708b80aab5
Christhian Gomez <chjugomez@utp.edu.co>
2017-01-28 10:57:41 -0500

    Were renamed some attributes and methods. Comments were added as well

--- 2048Game/2048.py
+++ 2048Game/2048.py
@@ -10,55 +10,55 @@ class _2048:
             [0,0,0,0],
             [0,0,0,0]
         ]
-        self.emptyCells = [] # will save coordinates empty cells '0'
-        self.key = ''   # user will give it for moves
-        self.number = 0 # number will appear randomly
-        self.addedNumbers = 0 # adding random number's during game
-        self.topNumber = 2048 # maximum number game
-        self.validMove = False # will check if exist a valid move
+        self.emptyCells = [] # will save empty cells coordinates ('0' -> [x,y])
+        self.key = ''   # will store user order for moves
+        self.num = 0 # number that will appear randomly
+        self.numsSum = 0 # will store appearing random number's add
+        self.topNum = 2048 # top number of game
+        self.validMove = False # will check if exist a valid move (putting or mixing numbers)
         self.firstMoves() # will begin putting two random numbers
 
-    def printBoard(self):
-        for item in self.board: print item,'\n'
+    def firstMoves(self): # will make the first move game
+        self.savEmptCells()
+        self.makeNum(),self.putNum()
+        self.makeNum(),self.putNum()
 
-    def firstMoves(self):
-        self.saveEmptyCells()
-        self.makeNumber(),self.putNumber()
-        self.makeNumber(),self.putNumber()
+    def printBoard(self): # will print board game
+        for _list in self.board: print _list,'\n'
 
-    def haveYouWin(self):
-        if self.addedNumbers >= self.topNumber:
-            for item in self.board:
-                if self.topNumber in item: return True
+    def haveYouWin(self): # will check if user won game
+        if self.numsSum >= self.topNum: # will begin to check only when necessary
+            for _list in self.board:
+                if self.topNum in _list: return True
 
-    def saveEmptyCells(self):
+    def savEmptCells(self): # will save empty cells coordinates from current board game
         rowCount = len(self.board)
         colCount = len(self.board[0])
         for _list,x in zip(self.board,range(0,rowCount)):
             for item,y in zip(_list,range(0,colCount)):
                 if not item: self.emptyCells.append([x,y])
 
-    def deleteEmptyCells(self):
+    def delEmptCells(self): # will delete empty cells coordinates from past board game
         self.emptyCells = []
 
-    def makeNumber(self):
-        self.number = rand.randrange(2,5,2)
-        self.addedNumbers += self.number
+    def makeNum(self): # will make 2 or 4 number randomly
+        self.num = rand.randrange(2,5,2)
+        self.numsSum += self.num
 
-    def putNumber(self):
+    def putNum(self): # will put 2 or 4 number into board random position
         sizeEmptyCells = len(self.emptyCells)
         if sizeEmptyCells:
             index = rand.randint(1,sizeEmptyCells)-1
             coordinates = self.emptyCells[index]
             x = coordinates[0]
             y = coordinates[1]
-            self.board[x][y] = self.number
+            self.board[x][y] = self.num
             del self.emptyCells[index]
 
-    def mixNumbers(self,_list):
-        indexes = Fifo() # Fifo queue, will save all indexes whose content be '0'
-        sizeList = len(_list)
-        for item,index in zip(_list,range(0,sizeList)):
+    def mixNumbs(self,_list): # will mix two equal consecutives numbers on given list from board game
+        indexes = Fifo() # fifo queue, will save indexes of list whose content be '0'
+        lenList = len(_list)
+        for item,index in zip(_list,range(0,lenList)):
             if item and indexes.size():
                 lastIndex = indexes.pop()
                 if item == _list[lastIndex]:
@@ -68,10 +68,10 @@ class _2048:
                 else: indexes.push(index)
             elif item: indexes.push(index)
 
-    def move(self,_list):
-        indexes = Fifo()
-        sizeList = len(_list)
-        for item,index in zip(_list,range(0,sizeList)):
+    def move(self,_list): # will move all numbers to left of given list from board game
+        indexes = Fifo() # fifo queue, will save indexes of list whose content be '0'
+        lenList = len(_list)
+        for item,index in zip(_list,range(0,lenList)):
             if item and indexes.size():
                 _list[indexes.pop()] = item
                 _list[index] = 0
@@ -79,39 +79,39 @@ class _2048:
                 self.validMove = True
             elif not item: indexes.push(index)
 
-    def exeKeyOrder(self):
+    def exeKeyOrder(self): # will perform the move into board game, ordered by user
         if self.key == 'a': # move leftward
-            for item in self.board:
-                self.mixNumbers(item)
-                self.move(item)
+            for _list in self.board:
+                self.mixNumbs(_list)
+                self.move(_list)
         elif self.key == 's': # move downward
             self.board = compl.transSquaredMatrix(self.board)
-            for item in self.board:
-                item.reverse()
-                self.mixNumbers(item)
-                self.move(item)
-                item.reverse()
+            for _list in self.board:
+                _list.reverse()
+                self.mixNumbs(_list)
+                self.move(_list)
+                _list.reverse()
             self.board = compl.transSquaredMatrix(self.board)
         elif self.key == 'd': # move rightward
-            for item in self.board:
-                item.reverse()
-                self.mixNumbers(item)
-                self.move(item)
-                item.reverse()
+            for _list in self.board:
+                _list.reverse()
+                self.mixNumbs(_list)
+                self.move(_list)
+                _list.reverse()
         elif self.key == 'w': # move upward
             self.board = compl.transSquaredMatrix(self.board)
-            for item in self.board:
-                self.mixNumbers(item)
-                self.move(item)
+            for _list in self.board:
+                self.mixNumbs(_list)
+                self.move(_list)
             self.board = compl.transSquaredMatrix(self.board)
-        if self.validMove:
-            self.deleteEmptyCells()
-            self.saveEmptyCells()
-            self.makeNumber()
-            self.putNumber()
+        if self.validMove: # will upgrade some game attributes, only when be necessary
+            self.delEmptCells()
+            self.savEmptCells()
+            self.makeNum()
+            self.putNum()
             self.validMove = False
 
-    def getKeyUser(self):
+    def getKeyUser(self): # will get user move order
         self.key = raw_input('Give me a key: ')
         self.key = self.key.lower()
 
