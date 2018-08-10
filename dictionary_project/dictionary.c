badd31d413a99f9129d6a9e3a8d07827db633f48
cjgc <chjugomez@utp.edu.co>
2018-08-09 21:16:27 -0500

    Improved dictionary builder version

--- dictionary_project/dictionary.c
+++ dictionary_project/dictionary.c
@@ -1,134 +1,176 @@
 #include <stdio.h>
 
-#define lenLine 255 /* lenght line */
-#define textLines 5000 /* total lines */
+/* lenght line */
+#define lenLine 255
 
-/* --------------- will clear jumps vector --------------- */
-void clearJumps(int jumps[],int size){
-	for(int i=0 ;i<size ;i++) jumps[i] = 0;
-}
+/* total lines */
+#define textLines 10000 
 
-/* --------------- will clear text file matrix --------------- */
-void clearTextFile(char TextFile[][lenLine]){
-	for(int i=0 ;i<textLines; i++)
-		for(int j=0 ;j<lenLine; j++) TextFile[i][j] = ' ';
-}
+/* --------------- it will load text on memory --------------- */
+ /* turn to uppercase char */
+void upper(char &readChar){readChar -= 32;}
 
-/* ---------------  will open text file --------------- */
-FILE * openFile(FILE *p){
-	char fileName[20];
-   do{
-      printf("file name: ");
-      scanf("%s",fileName);
-      p = fopen(fileName,"r");
-   }while(!p);
-	return p;
+/* turn to lowercase char */
+void lower(char &readChar){readChar += 32;}
+
+/* check if is a valid line */
+bool isValidLine(char M[][lenLine],int i, int j) {
+  for(int k=0; k<=j; k++) {
+    if(M[i][k] == ' ') continue;
+    if(M[i][k] == '\n') return false; 
+    return true;
+  } 
 }
 
-/* --------------- will load text on memory --------------- */
-void upper(char &readChar){readChar -= 32;} /* upper case char */
-void lower(char &readChar){readChar += 32;} /* lower case char */
-void loadText(FILE *text,char M[][lenLine],int &loadedLines){ /* will load text */
-	char readChar = ' ';
-	for(int i=0; i<textLines; i++)
-		for(int j=0; j<lenLine; j++){
-			readChar = fgetc(text);
-			if(!j && readChar >= 97 && readChar <= 122) upper(readChar);
-			else if(j && readChar >= 65 && readChar <= 90) lower(readChar);
-			else if(!j && readChar == ' ') readChar = '@';
-			else if(!j && readChar == '\n'){i--;break;}
-			M[i][j] = readChar;
-			if(feof(text)) return;
-			if(readChar == '\n'){loadedLines++; break;}
-		}
+/* it will load text into system memory */
+void loadText(FILE *text,char M[][lenLine],int &loadedLines) {
+  for(int i=0,j=0; (i<textLines && !feof(text));) { 
+    char readChar = ' ';
+    int k = 0;
+    for(j=0; (j<lenLine && !feof(text) && readChar != '\n'); j++) {
+      readChar = fgetc(text);
+      if(readChar >= 65 && readChar <= 90) lower(readChar);
+      M[i][j] = readChar;
+      k = j;
+    } 
+
+    if(isValidLine(M,i,k)) {
+      loadedLines++; 
+      if(M[i][0] >= 97 &&  M[i][0] <= 122) upper(M[i][0]);
+      i++;
+    }
+ 
+  }
 }
 
-/* --------------- will sort text with quicksort method --------------- */
-void swap(char M[][lenLine],int left,int right){ /* will swap two lines on M[]*/
-	char readChar = ' ';
-	for(int j=0; j<lenLine; j++){
-		readChar = M[left][j];
-		M[left][j] = M[right][j];
-		M[right][j] = readChar;
-	}
-}
-void getPivot(char M[][lenLine],char pivot[],int left,int right){ /* will get pivot from M[] */
-	int indexPivot = (left+right)/2;
-	for(int j=0; j<lenLine; j++) pivot[j] = M[indexPivot][j];
-}
-bool lesserPivot(char M[][lenLine],char pivot[],int left){ /* check if a word is lesser than pivot */
-	for(int j=0; j<lenLine; j++){
-		if(M[left][j] < pivot[j]) return true;
-		if(M[left][j] > pivot[j] || M[left][j] == '\n' && pivot[j] == '\n') return false;
-	}
-}
-bool greaterPivot(char M[][lenLine],char pivot[],int right){ /* check if a word is greater than pivot */
-	for(int j=0; j<lenLine; j++){
-		if(M[right][j] > pivot[j]) return true;
-		if(M[right][j] < pivot[j] || M[right][j] == '\n' && pivot[j] == '\n') return false;
-	}
-}
-void quickSort(char M[][lenLine],int left,int right){ /* quick sort method */
-	if(left >= right) return;
-	int l = left, r = right;
-	char pivot[lenLine];
-	getPivot(M,pivot,left,right);
-	while(l <= r){
-		while(lesserPivot(M,pivot,l)) l++;
-		while(greaterPivot(M,pivot,r)) r--;
-		if(l < r){
-			swap(M,l,r);
-			l++;r--;
-		}else if(l == r){l++;r--;}
-	}
-	quickSort(M,left,r);
-	quickSort(M,l,right);
+/* --------------- it will sort text with quicksort method --------------- */
+/* it will swap two lines on M[]*/
+void swap(char M[][lenLine],int left,int right) {
+  char readChar = ' ';
+  for(int j=0; j<lenLine; j++) {
+    readChar = M[left][j];
+    M[left][j] = M[right][j];
+    M[right][j] = readChar;
+  }
 }
 
-/* --------------- will detect repeated words --------------- */
-void readWord(char M[][lenLine],char word[],int position){ /* will read a word from textFile matrix */
-	for(int j=0 ;j<lenLine; j++) word[j] = M[position][j];
-}
-bool isRepeatedWord(char word1[],char word2[]){ /* will check if word1 is equal to word2 */
-	for(int j=0 ;j<lenLine; j++) if(word1[j] != word2[j]) return false;
-	return true;
-}
-void seekRepeatedWords(char M[][lenLine],int jumps[],int loadedLines){ /* will seek repeated words */
-	char word1[lenLine], word2[lenLine];
-	for(int i=0; i<loadedLines; i++){
-		readWord(M,word1,i);
-		for(int z=i+1; z<loadedLines;){
-			readWord(M,word2,z);
-			if(isRepeatedWord(word1,word2)){
-				jumps[i] += 1;
-				z++;
-			}else break;
-		}
-		i += jumps[i];
-	}
+/* it will set the pivot from M[] */
+void setPivot(char M[][lenLine],char pivot[],int left,int right) {
+  int indexPivot = (left+right)/2;
+  for(int j=0; j<lenLine; j++) pivot[j] = M[indexPivot][j];
 }
 
-/* --------------- will write words on hard drive --------------- */
-void diskWriting(FILE *disk,char M[][lenLine],int jumps[],int loadedLines){
-	for(int i=0; i<loadedLines; i++){
-		for(int j=0; j<lenLine; j++){
-			fputc(M[i][j],disk);
-			if(M[i][j] == '\n') break;
-		}
-		i += jumps[i];
-	}
+/* it will check if a word is lesser than pivot */
+bool lesserPivot(char M[][lenLine],char pivot[],int left) { 
+  for(int j=0; j<lenLine; j++) {
+    if(M[left][j] < pivot[j]) return true;
+    if(M[left][j] > pivot[j] || 
+       M[left][j] == '\n' || pivot[j] == '\n') 
+      return false;
+  }
 }
 
-int main(){
-	char textFile[textLines][lenLine];
-	FILE *text=openFile(text), *disk=fopen("output.txt","w");
-	int loadedLines=0, jumps[textLines];
-	clearTextFile(textFile);
-	clearJumps(jumps,textLines);
-	loadText(text,textFile,loadedLines);
-	quickSort(textFile,0,loadedLines-1);
-	seekRepeatedWords(textFile,jumps,loadedLines);
-	diskWriting(disk,textFile,jumps,loadedLines);
-	fclose(text);
-	fclose(disk);
+/* it will check if a word is greater than pivot */
+bool greaterPivot(char M[][lenLine],char pivot[],int right) {
+  for(int j=0; j<lenLine; j++) {
+    if(M[right][j] > pivot[j]) return true;
+    if(M[right][j] < pivot[j] ||
+       M[right][j] == '\n' || pivot[j] == '\n')
+      return false;
+  }
+}
+
+/* it will perform quick sort algorithm */
+void quickSort(char M[][lenLine],int left,int right) {
+  if(left >= right) return;
+  int l = left, r = right;
+  char pivot[lenLine];
+  setPivot(M,pivot,left,right);
+
+  while(l <= r) {
+    while(lesserPivot(M,pivot,l)) l++;
+    while(greaterPivot(M,pivot,r)) r--;
+    if(l < r) {
+      swap(M,l,r);
+      l++;r--;
+    }
+    else if(l == r) {l++;r--;}
+  }
+
+  quickSort(M,left,r);
+  quickSort(M,l,right);
+}
+
+/* --------------- it will write words on hard drive --------------- */
+/* it will clear line array */
+void clearLine(char line[]) {
+  for(int j=0; j<lenLine; j++)
+    line[j] = ' ';
+}
+
+/* it will compare two lines */
+bool areEqual(char M[][lenLine],int i,char line2[]) {
+  for(int j=0; j<lenLine; j++) {
+    if(M[i][j] != line2[j]) return false;
+    if(M[i][j] == '\n' && line2[j] == '\n') return true;
+  }
+}
+
+/* it will write a line on hard drive */
+void writeToDisk(FILE *disk,char M[][lenLine],int i) {
+  for(int j=0; j<lenLine; j++) {
+    fputc(M[i][j],disk);
+    if(M[i][j] == '\n') break;
+  }
+}
+
+/* it will swap two lines */
+void swapLine(char M[][lenLine],int i,char line2[]) {
+  for(int j=0; j<lenLine; j++) {
+    line2[j] = M[i][j];
+    if(M[i][j] == '\n') break;
+  }
+}
+
+/* it will manage the process for hard drive writing */
+void diskWriting(FILE *disk,char M[][lenLine],int loadedLines) {
+  char previousLine[lenLine];
+  clearLine(previousLine);
+  for(int i=0; i<loadedLines; i++) { 
+    if(!areEqual(M,i, previousLine)) {
+      writeToDisk(disk,M,i); 
+      swapLine(M,i,previousLine);
+    }
+  }
+}
+
+void printLine(char M[][lenLine],int i) {
+  for(int j=0; j<lenLine; j++) 
+    printf("%c",M[i][j]);
+  printf("\n");
+}
+
+int main(int argc, char **argv) {
+  if(argc != 2) {
+    printf("Enter a valid filename <<%s filename.txt>>\n",argv[0]);
+    return -1;
+  }
+  
+  char textFile[textLines][lenLine];
+  FILE *text=fopen(argv[1],"r"), *disk=fopen("output.txt","w");
+  
+  if(!text) {
+    printf("Was not possible open the file\n");
+    fclose(disk);
+    return -1;
+  }
+
+  int loadedLines=0;
+  loadText(text,textFile,loadedLines);
+  printf("%d\n",loadedLines);
+  quickSort(textFile,0,loadedLines-1);
+  //clearMatrix(textFile,loadedLines);
+  diskWriting(disk,textFile,loadedLines);
+  fclose(text);
+  fclose(disk);
 }
